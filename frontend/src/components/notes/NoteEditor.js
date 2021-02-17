d256df9fefc9774d722384449a416c5241c2f924
jpaul121 <jpaulvalen+github@gmail.com>
2021-02-17 17:21:32 -0500

    Added basic NoteEditor to NotebookView

--- frontend/src/components/notes/NoteEditor.js
+++ frontend/src/components/notes/NoteEditor.js
@@ -1,10 +1,13 @@
-import 'react-quill/dist/quill.snow.css'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 import 'regenerator-runtime/runtime.js'
 
+import { ContentState, EditorState, convertFromRaw, convertToRaw } from 'draft-js'
 import React, { Component } from 'react'
-import ReactQuill, { Quill } from 'react-quill'
 
+import { Editor } from 'react-draft-wysiwyg'
 import axios from 'axios'
+import drafttoHtml from 'draftjs-to-html'
+import htmlToDraft from 'html-to-draftjs'
 import styles from './NoteEditor.module.css'
 import { withRouter } from 'react-router'
 
@@ -12,8 +15,9 @@ class NoteEditor extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      content: null,
+      contentState: null,
       isLoading: true,
+      noteID: null,
     }
   }
 
@@ -28,30 +32,51 @@ class NoteEditor extends Component {
     const response = await axios.get(`/api/notes/${note_id}`)
     const content = response.data.content
 
-    // Yes, the author is aware this doesn't return anything. 
-    return this.setState({
-      content,
-      isLoading: false,
+    // Yes, the author is aware this.setState doesn't return anything. 
+    if (content) {
+      const contentState = convertFromRaw(content)
+      
+      return this.setState({
+        contentState,
+        isLoading: false,
+        noteID: note_id,
+      });
+    } else {
+      return this.setState({
+        contentState: ContentState.createFromText('', ''),
+        isLoading: false,
+        noteID: note_id,
+      });
+    }
+  }
+
+  onContentStateChange(contentState) {
+    this.setState({
+      contentState,
     });
   }
 
-  onChange(content, delta, source, editor) {
-    const text = editor.getText(content)
-    
-    this.setState({
-      content: text,
-    });
+  saveNote() {
+    axios.post(`/api/${this.state.noteID}`, {
+      content: convertToRaw(this.contentState),
+    })
   }
 
   render() {
     return !this.state.isLoading && (
       <div>
-        <ReactQuill
-          className={styles['editor']}
-          placeholder={'Write something...'}
-          ref={'editor'}
-          value={this.state.content}
+        <Editor
+          editorState={EditorState.createWithContent(this.state.contentState)}
+          wrapperClassName='editor-wrapper'
+          editorClassName='text-editor'
+          onContentStateChange={this.onContentStateChange}
         />
+        <button 
+          type='button' 
+          onClick={this.saveNote}
+        >
+          Save
+        </button>
       </div>
     );
   }
