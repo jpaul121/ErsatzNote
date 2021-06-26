7297429d49ad8a13ffb2bac7aac4449fb76b0203
jpaul121 <jpaulvalen+github@gmail.com>
2021-06-25 23:48:03 -0400

    Set debug mode to false for initial deployment

--- core/settings.py
+++ core/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ 'localhost', '.herokuapp.com', 'ersatznote.com' ]
+ALLOWED_HOSTS = [ 'localhost', 'ersatznote.herokuapp.com', 'ersatznote.com' ]
 
 # Application definition
 
